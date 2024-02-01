a471268e744e9f7eeb00556003daeccf2da71196
danischm <danischm@cisco.com>
2024-02-01 19:03:49 +0100

    Update repos with pipeline

--- Jenkinsfile
+++ Jenkinsfile
@@ -8,6 +8,8 @@ pipeline {
     }
 
     environment {
+        DD_GITHUB_TOKEN = credentials('DD_GITHUB_TOKEN')
+        DD_INTERNAL_GITHUB_TOKEN = credentials('DD_INTERNAL_GITHUB_TOKEN')
         WEBEX_TOKEN = credentials('WEBEX_TOKEN')
         WEBEX_ROOM_ID = 'Y2lzY29zcGFyazovL3VzL1JPT00vNTFmMGNmODAtYjI0My0xMWU5LTljZjUtNWY0NGQ2ZTlmYWY0'
         GIT_COMMIT_MESSAGE = "${sh(returnStdout: true, script: 'git config --global --add safe.directory "*" && git log -1 --pretty=%B ${GIT_COMMIT}').trim()}"
@@ -33,6 +35,11 @@ pipeline {
 
     post {
         always {
+            script {
+                if (env.BRANCH_NAME == 'master') {
+                    sh 'cd scripts && python3 update_repos.py'
+                }
+            }
             sh "BUILD_STATUS=${currentBuild.currentResult} python .ci/webex-notification-jenkins.py"
         }
     }
---
+++ scripts/update_repos.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2023, Daniel Schmidt <danischm@cisco.com>
+
+import os
+import shutil
+import subprocess
+import tempfile
+
+REPOS = [
+    {
+        "url": "https://{}@wwwin-github.cisco.com/AS-Customer/sdwanac.git",
+        "type": "internal",
+        "commit_message": "Nac sdwan updates",
+        "directories": [
+            {
+                "src": "../validation/rules",
+                "dst": "./validation/rules",
+            },
+        ],
+        "files": [
+            {
+                "src": "../schemas/sdwan.yaml",
+                "dst": "./schemas/sdwan.yaml",
+            },
+            {
+                "src": "../defaults/sdwan.yaml",
+                "dst": "./defaults/sdwan.yaml",
+            },
+        ],
+    },
+    {
+        "url": "https://{}@wwwin-github.cisco.com/netascode/nac-sdwan-example.git",
+        "type": "internal",
+        "commit_message": "Nac sdwan updates",
+        "directories": [
+            {
+                "src": "../validation/rules",
+                "dst": "./.rules",
+            },
+        ],
+        "files": [
+            {
+                "src": "../schemas/sdwan.yaml",
+                "dst": "./.schema.yaml",
+            },
+        ],
+    },
+    {
+        "url": "https://{}@github.com/netascode/terraform-sdwan-nac-sdwan.git",
+        "type": "external",
+        "commit_message": "Nac sdwan updates",
+        "files": [
+            {
+                "src": "../defaults/sdwan.yaml",
+                "dst": "./defaults/sdwan.yaml",
+            },
+        ],
+    },
+]
+
+
+def print_message(message):
+    print(
+        "--------------------------------------------------------------------------------"
+    )
+    print(message)
+    print(
+        "--------------------------------------------------------------------------------"
+    )
+
+
+def update_repo(repo):
+    with tempfile.TemporaryDirectory() as dirname:
+        if repo["type"] == "internal":
+            url = repo["url"].format(os.getenv("DD_INTERNAL_GITHUB_TOKEN"))
+        elif repo["type"] == "external":
+            url = repo["url"].format(os.getenv("DD_GITHUB_TOKEN"))
+        args = ["git", "clone", url, dirname]
+        print_message("git clone")
+        subprocess.run(args, check=True)
+        # copy files and dirs
+        for dir in repo.get("directories", []):
+            shutil.copytree(
+                dir["src"], os.path.join(dirname, dir["dst"]), dirs_exist_ok=True
+            )
+        for file in repo.get("files", []):
+            shutil.copyfile(file["src"], os.path.join(dirname, file["dst"]))
+        cwd = dirname
+        args = ["git", "add", "--all"]
+        print_message(args)
+        subprocess.run(args, cwd=cwd, check=True)
+        args = ["git", "diff", "--cached", "--exit-code"]
+        print_message(args)
+        r = subprocess.run(args, cwd=cwd)
+        if r.returncode > 0:
+            if repo["type"] == "internal":
+                subprocess.run(
+                    ["git", "config", "user.email", "digidev.gen@cisco.com"],
+                    cwd=cwd,
+                    check=True,
+                )
+                subprocess.run(
+                    ["git", "config", "user.name", "digidev.gen"], cwd=cwd, check=True
+                )
+            elif repo["type"] == "external":
+                subprocess.run(
+                    ["git", "config", "user.email", "netascode-gen@cisco.com"],
+                    cwd=cwd,
+                    check=True,
+                )
+                subprocess.run(
+                    ["git", "config", "user.name", "netascode-gen"], cwd=cwd, check=True
+                )
+            args = ["git", "commit", "-m", repo["commit_message"]]
+            print_message(args)
+            subprocess.run(args, cwd=cwd, check=True)
+            args = ["git", "push"]
+            print_message(args)
+            subprocess.run(args, cwd=cwd, check=True)
+
+
+def update_repos():
+    for repo in REPOS:
+        print("\n-> Updating repo {}\n".format(repo["url"]))
+        update_repo(repo)
+
+
+if __name__ == "__main__":
+    update_repos()
