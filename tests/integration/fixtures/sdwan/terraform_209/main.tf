51158789516db17ac61fbda6787490c65fe1e136
danischm <danischm@cisco.com>
2024-04-18 11:59:20 +0200

    Add defaults to testing

--- tests/integration/fixtures/sdwan/terraform_209/main.tf
+++ tests/integration/fixtures/sdwan/terraform_209/main.tf
@@ -1,6 +1,7 @@
 module "sdwan" {
-  
   source = "git::https://github.com/netascode/terraform-sdwan-nac-sdwan.git?ref=main"
+
   yaml_directories = ["../standard"]
+  write_default_values_file = "defaults.yaml"
 }
 
