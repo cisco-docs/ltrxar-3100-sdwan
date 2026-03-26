8432cd6e686a1debe0347d3a17fd09efcc498417
USPS Admin <uspsadmin@cisco.com>
2026-03-26 17:19:12 +0000

    Admin Run v7

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
-module "sdwan" {
-  source  = "netascode/nac-sdwan/sdwan"
-  version = "1.3.0"
-
-  yaml_directories = ["data/"]
-
-  write_default_values_file = "defaults.yaml"
-}
-
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     sdwan = {
-      source = "CiscoDevNet/sdwan"
-      version = "0.11.0"
+      source  = "CiscoDevNet/sdwan"
+      version = "~> 0.11.0"
     }
   }
   backend "http" {
     skip_cert_verification = true
   }
+}
+
+module "sdwan" {
+  source  = "netascode/nac-sdwan/sdwan"
+  version = "~> 1.3.0"
+
+  yaml_directories = ["data/"]
+
+  write_default_values_file = "defaults.yaml"
 }
